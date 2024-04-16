5ef1fb8349a8373059b0d3a22dd03381dc4337db
markfrancis <markfrancispgorreon@iskolarngbayan.pup.edu.ph>
2024-04-16 19:19:58 +0800

    Create logs.php
    Added a logs page for vehicle entry records

---
+++ public/view/admin/logs.php
@@ -0,0 +1,127 @@
+<?php
+include_once 'header.php';
+?>
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Records Logs</h1>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row col-12">
+        <!-- Area Chart -->
+        <div class="col-12">
+            <div class="card mb-8">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>Address</th>
+                                    <th>Vehicle Type</th>
+                                    <th>Plate Number</th>
+                                    <th>QR Code</th>
+                                    <th>Date and Time</th>
+                                    <th>Entry / Exit</th>
+
+                                </tr>
+                            </thead>
+
+                            <tbody>
+
+                                <tr>
+                                    <td>Peony St</td>
+                                    <td>Sedan</td>
+                                    <td>NED-5724</td>
+                                    <td>XXXXXYYYYY</td>
+                                    <td>April 16, 2024</td>
+                                    <td>Entry</td>
+                                </tr>
+                                <tr>
+                                    <td>Peony St</td>
+                                    <td>Sedan</td>
+                                    <td>NED-5724</td>
+                                    <td>XXXXXYYYYY</td>
+                                    <td>April 16, 2024</td>
+                                    <td>Entry</td>
+                                </tr>
+                                <tr>
+                                    <td>Peony St</td>
+                                    <td>Sedan</td>
+                                    <td>NED-5724</td>
+                                    <td>XXXXXYYYYY</td>
+                                    <td>April 16, 2024</td>
+                                    <td>Entry</td>
+                                </tr>
+                                <tr>
+                                    <td>Peony St</td>
+                                    <td>Sedan</td>
+                                    <td>NED-5724</td>
+                                    <td>XXXXXYYYYY</td>
+                                    <td>April 16, 2024</td>
+                                    <td>Entry</td>
+                                </tr>
+                                <tr>
+                                    <td>Peony St</td>
+                                    <td>Sedan</td>
+                                    <td>NED-5724</td>
+                                    <td>XXXXXYYYYY</td>
+                                    <td>April 16, 2024</td>
+                                    <td>Entry</td>
+                                </tr>
+                                <tr>
+                                    <td>Peony St</td>
+                                    <td>Sedan</td>
+                                    <td>NED-5724</td>
+                                    <td>XXXXXYYYYY</td>
+                                    <td>April 16, 2024</td>
+                                    <td>Entry</td>
+                                </tr>
+                                <tr>
+                                    <td>Peony St</td>
+                                    <td>Sedan</td>
+                                    <td>NED-5724</td>
+                                    <td>XXXXXYYYYY</td>
+                                    <td>April 16, 2024</td>
+                                    <td>Entry</td>
+                                </tr>
+                                <tr>
+                                    <td>Peony St</td>
+                                    <td>Sedan</td>
+                                    <td>NED-5724</td>
+                                    <td>XXXXXYYYYY</td>
+                                    <td>April 16, 2024</td>
+                                    <td>Entry</td>
+                                </tr>
+                                <tr>
+                                    <td>Peony St</td>
+                                    <td>Sedan</td>
+                                    <td>NED-5724</td>
+                                    <td>XXXXXYYYYY</td>
+                                    <td>April 16, 2024</td>
+                                    <td>Entry</td>
+                                </tr>
+                                <tr>
+                                    <td>Peony St</td>
+                                    <td>Sedan</td>
+                                    <td>NED-5724</td>
+                                    <td>XXXXXYYYYY</td>
+                                    <td>April 16, 2024</td>
+                                    <td>Entry</td>
+                                </tr>
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+<?php
+include_once 'footer.php';
+?>

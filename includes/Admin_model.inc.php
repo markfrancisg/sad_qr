3e379532226c9e5eb95cb1ca9c5fb61840a4e552
markfrancis <markfrancispgorreon@iskolarngbayan.pup.edu.ph>
2024-04-17 10:53:15 +0800

    Fixed the logic when qr code expires

--- includes/Admin_model.inc.php
+++ includes/Admin_model.inc.php
@@ -185,7 +185,7 @@ function get_all_registered_vehicle(object $pdo)
 
 function update_registration_status(object $pdo, int $qr_id)
 {
-    $query = "UPDATE qr_info SET registered = 0 WHERE qr_id = :qr_id";
+    $query = "UPDATE qr_info SET registered = 0, qr_code = 'Not Registered', expiration_date = NULL WHERE qr_id = :qr_id";
     $stmt2 = $pdo->prepare($query);
     $stmt2->bindParam(":qr_id", $qr_id);
 

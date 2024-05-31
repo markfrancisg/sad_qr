93fd0a820be7e6ce367384aebc28926e880a1ac9
markfrancis <markfrancispgorreon@iskolarngbayan.pup.edu.ph>
2024-06-01 05:33:38 +0800

    added hover effect in the admin dashboard

--- public/view/admin/admin.dashboard.php
+++ public/view/admin/admin.dashboard.php
@@ -13,6 +13,17 @@ $total_unpaid = count_unpaid_vehicles($pdo);
 
 ?>
 
+<style>
+    .hover-grow {
+        transition: transform 0.3s ease;
+    }
+
+    .hover-grow:hover {
+        transform: scale(1.10);
+        /* Adjust the scale value as needed */
+    }
+</style>
+
 <!--  Header End -->
 <div class="container-fluid">
     <div class="container-fluid">
@@ -28,40 +39,48 @@ $total_unpaid = count_unpaid_vehicles($pdo);
 
                 <div class="row">
                     <div class="col-sm-12 col-md-6 col-lg-3">
-                        <div class="card bg-success">
-                            <div class="card-body">
-                                <h5 class="text-light text-center">Homeowners</h5>
-                                <h1 class="text-light text-center"><?php echo $total_homeowners ?></h1>
-                                <img src="../../images/2.svg" class="img-fluid d-block mx-auto" width="80" alt="...">
+                        <a href="homeowner_list.php">
+                            <div class="card bg-success hover-grow">
+                                <div class="card-body">
+                                    <h5 class="text-light text-center">Homeowners</h5>
+                                    <h1 class="text-light text-center"><?php echo $total_homeowners ?></h1>
+                                    <img src="../../images/2.svg" class="img-fluid d-block mx-auto" width="80" alt="...">
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <div class="col-sm-12 col-md-6 col-lg-3">
-                        <div class="card bg-success">
-                            <div class="card-body">
-                                <h5 class="text-light text-center">Total Vehicles</h5>
-                                <h1 class="text-light text-center"><?php echo $total_vehicles ?></h1>
-                                <img src="../../images/1.svg" class="img-fluid d-block mx-auto" width="80" alt="...">
+                        <a href="vehicle_list.php">
+                            <div class="card bg-success hover-grow">
+                                <div class="card-body">
+                                    <h5 class="text-light text-center">Total Vehicles</h5>
+                                    <h1 class="text-light text-center"><?php echo $total_vehicles ?></h1>
+                                    <img src="../../images/1.svg" class="img-fluid d-block mx-auto" width="80" alt="...">
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <div class="col-sm-12 col-md-6 col-lg-3">
-                        <div class="card bg-success">
-                            <div class="card-body">
-                                <h5 class="text-light text-center">Paid Vehicles</h5>
-                                <h1 class="text-light text-center"><?php echo $total_paid ?></h1>
-                                <img src="../../images/1.svg" class="img-fluid d-block mx-auto" width="80" alt="...">
+                        <a href="vehicle_list_paid.php">
+                            <div class="card bg-success hover-grow">
+                                <div class="card-body">
+                                    <h5 class="text-light text-center">Paid Vehicles</h5>
+                                    <h1 class="text-light text-center"><?php echo $total_paid ?></h1>
+                                    <img src="../../images/1.svg" class="img-fluid d-block mx-auto" width="80" alt="...">
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <div class="col-sm-12 col-md-6 col-lg-3">
-                        <div class="card bg-success">
-                            <div class="card-body">
-                                <h5 class="text-light text-center">Unpaid Vehicles</h5>
-                                <h1 class="text-light text-center"><?php echo $total_unpaid ?></h1>
-                                <img src="../../images/1.svg" class="img-fluid d-block mx-auto" width="80" alt="...">
+                        <a href="vehicle_list_unpaid.php">
+                            <div class="card bg-success hover-grow">
+                                <div class="card-body">
+                                    <h5 class="text-light text-center">Unpaid Vehicles</h5>
+                                    <h1 class="text-light text-center"><?php echo $total_unpaid ?></h1>
+                                    <img src="../../images/1.svg" class="img-fluid d-block mx-auto" width="80" alt="...">
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
 
                 </div>
@@ -69,7 +88,7 @@ $total_unpaid = count_unpaid_vehicles($pdo);
                 <div class="row">
                     <div class="col-sm-6 col-md-6 col-lg-6">
                         <div class="card">
-                            <div class="card-body">
+                            <div class="card-body ">
                                 <h5 class="card-title text-center">Logs This Day</h5>
                                 <h1 class="text-dark text-center">350</h1>
                                 <img src="../../images/2.svg" class="img-fluid d-block mx-auto" width="100" alt="...">

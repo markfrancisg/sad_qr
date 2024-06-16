ae22134ab07f553c82daf8018446975d61618ef9
markfrancis <markfrancispgorreon@iskolarngbayan.pup.edu.ph>
2024-06-17 00:02:21 +0800

    Edited the search function for logs admin side

--- includes/LogsListDailyController.php
+++ includes/LogsListDailyController.php
@@ -1,21 +1,33 @@
 <?php
+try {
+    if (isset($_GET['page_no']) && $_GET['page_no'] != "") {
+        $page_no = $_GET['page_no'];
+    } else {
+        $page_no = 1;
+    }
 
-if (isset($_GET['page_no']) && $_GET['page_no'] != "") {
-    $page_no = $_GET['page_no'];
-} else {
-    $page_no = 1;
+    $total_records_per_page = 15;
+
+    $offset = ($page_no - 1) * $total_records_per_page;
+
+    $previous_page = $page_no - 1;
+
+    $next_page = $page_no + 1;
+
+    $total_records = count_logs_list_daily($pdo);
+
+    $total_no_of_pages = ceil($total_records / $total_records_per_page);
+
+    $results = get_logs_list_daily($pdo, $offset, $total_records_per_page);
+
+    if (isset($_GET['searchButton'])) {
+        // Get the search input value
+        $searchInput = isset($_GET['searchInput']) ? $_GET['searchInput'] : '';
+
+        $results = search_daily($pdo, $searchInput);
+    }
+
+    $searchPerformed = isset($_GET['searchButton']);
+} catch (PDOException $e) {
+    die("Query failed:" . $e->getMessage());
 }
-
-$total_records_per_page = 15;
-
-$offset = ($page_no - 1) * $total_records_per_page;
-
-$previous_page = $page_no - 1;
-
-$next_page = $page_no + 1;
-
-$total_records = count_logs_list_daily($pdo);
-
-$total_no_of_pages = ceil($total_records / $total_records_per_page);
-
-$results = get_logs_list_daily($pdo, $offset, $total_records_per_page);

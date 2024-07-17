ff7b12b51cb39e5709d562e4e534c608ee43b889
markfrancis <markfrancispgorreon@iskolarngbayan.pup.edu.ph>
2024-07-18 02:14:53 +0800

    FIxed the database

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,14 +1,14 @@
 <?php
 //connect to database
-// $host = "localhost";
-// $dbname = "u769397434_sad_seqrity";
-// $dbusername = "u769397434_sanlorenzo";
-// $dbpassword = "Aldetek15";
-
 $host = "localhost";
-$dbname = "sad_seqrity";
-$dbusername = "root";
-$dbpassword = "";
+$dbname = "u769397434_sad_seqrity";
+$dbusername = "u769397434_sanlorenzo";
+$dbpassword = "Aldetek15";
+
+// $host = "localhost";
+// $dbname = "sad_seqrity";
+// $dbusername = "root";
+// $dbpassword = "";
 
 try {
     $pdo = new PDO("mysql:host=$host; dbname=$dbname", $dbusername, $dbpassword);
